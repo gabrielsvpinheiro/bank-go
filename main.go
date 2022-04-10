e3388389d8dbd879d79cc2c4bef18a72319205dc
Gabriel <gabriel.silva.pinheiro2@gmail.com>
2022-04-10 19:00:56 -0300

    struct

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
+type ContaCorrente = struct {
+	titular string
+	numeroAgencia int 
+	numeroConta int 
+	saldo float64 
+}
+
 func main() {
-	
+	contaDoBruno := ContaCorrente{"Bruno",123, 123456, 100.00}
+	contaDoThiago := ContaCorrente{"Thiago", 123, 123457, 150.00}
+	fmt.Println(contaDoBruno)
+	fmt.Println(contaDoThiago)
 }
