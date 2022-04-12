4b5e4e1ef95935aabe2e142231f7f35ccd5dd9b0
Gabriel <gabriel.silva.pinheiro2@gmail.com>
2022-04-11 21:37:41 -0300

    depositar-sacar

--- main.go
+++ main.go
@@ -2,16 +2,40 @@ package main
 
 import "fmt"
 
-type ContaCorrente = struct {
-	titular string
-	numeroAgencia int 
-	numeroConta int 
-	saldo float64 
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) string {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso"
+	} else {
+		return "Valor do deposito inválido"
+	}
 }
 
 func main() {
-	contaDoBruno := ContaCorrente{"Bruno",123, 123456, 100.00}
-	contaDoThiago := ContaCorrente{"Thiago", 123, 123457, 150.00}
-	fmt.Println(contaDoBruno)
-	fmt.Println(contaDoThiago)
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
+
+	fmt.Println(contaDaSilvia.saldo)
+
+	fmt.Println(contaDaSilvia.Depositar(-400))
+	fmt.Println(contaDaSilvia.saldo)
+
 }
